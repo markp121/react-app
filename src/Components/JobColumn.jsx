21979096d75635a70ffa9b3d3e837017695c0776
Mark Powell <mpowell1219@gmail.com>
2025-07-18 17:27:39 +0100

    Added placeholder message in each column when there are no jobs

--- src/Components/JobColumn.jsx
+++ src/Components/JobColumn.jsx
@@ -1,21 +1,31 @@
-import React, {useEffect, useMemo} from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 const JobColumn = ({ jobStatus, statusList, setStatusList, jobsState, searchText }) => {
+  const [emptyList, setEmptyList] = useState(true);
+
   useEffect(() => {
     setStatusList(jobsState.filter((job) => job.status === jobStatus));
   }, [setStatusList, jobsState, jobStatus]);
 
-    const filteredJobsList = useMemo(() => {
-        return statusList.filter(
-            (statusListItem) =>
-                statusListItem.name.toLowerCase().includes(searchText.toLowerCase()),
-        );
-    }, [searchText, statusList]);
+  const filteredJobsList = useMemo(() => {
+    return statusList.filter((statusListItem) =>
+      statusListItem.name.toLowerCase().includes(searchText.toLowerCase()),
+    );
+  }, [searchText, statusList]);
+
+  useEffect(() => {
+    if (filteredJobsList.length > 0) {
+      setEmptyList(false);
+    }
+  }, [filteredJobsList]);
 
   return (
     <div className="job-column">
       <h2>{jobStatus}</h2>
-      <ul className="job-list">
+      <ul className="job-column-list">
+        {emptyList && (
+            <p className="placeholder-message">There are no {jobStatus} jobs scheduled.</p>
+        )}
         {filteredJobsList.map((job) => (
           <li className={`job-column-list-item ${job.status}`} key={job.id}>
             <div className="job-info">
